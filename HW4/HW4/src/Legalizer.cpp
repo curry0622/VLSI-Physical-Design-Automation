2a44a18ba54ed1b9a01399027e7db0cdd6998a9a
curry0622 <kingkazma0112305@gmail.com>
2022-12-20 15:28:43 +0800

    feat: read node

--- HW4/HW4/src/Legalizer.cpp
+++ HW4/HW4/src/Legalizer.cpp
@@ -9,25 +9,81 @@ Legalizer::Legalizer(std::string input_file, std::string output_file) {
     read_input(input_file);
 }
 
+void Legalizer::read_node(std::string node_file) {
+    // Variables
+    std::ifstream file(node_file);
+    std::stringstream ss;
+    std::string line, buffer;
+
+    // Read info
+    std::getline(file, line);
+    std::cout << line << std::endl;
+    ss << line;
+    ss >> buffer >> buffer >> num_nodes;
+    ss.clear();
+
+    std::getline(file, line);
+    std::cout << line << std::endl;
+    ss << line;
+    ss >> buffer >> buffer >> num_blockages;
+    ss.clear();
+
+    // Read cells
+    num_cells = num_nodes - num_blockages;
+    std::getline(file, line);
+    for(int i = 0; i < num_cells; i++) {
+        std::getline(file, line);
+        std::string name;
+        int w, h;
+        ss << line;
+        ss >> name >> w >> h;
+        ss.clear();
+        std::cout << name << ": " << w << ", " << h << std::endl;
+    }
+
+    // Read blockages
+    for(int i = 0; i < num_blockages; i++) {
+        std::getline(file, line);
+        std::string name;
+        int w, h;
+        ss << line;
+        ss >> name >> w >> h >> buffer;
+        ss.clear();
+        std::cout << name << ": " << w << ", " << h << std::endl;
+    }
+}
+
+void Legalizer::read_pl(std::string node_file) {
+    
+}
+
+void Legalizer::read_scl(std::string node_file) {
+    
+}
+
 void Legalizer::read_input(std::string input_file) {
+    // Parse file path
+    std::string path = input_file.substr(0, input_file.find_last_of("/\\") + 1);
+    std::cout << "path: " << path << std::endl;
+
     // Variables
     std::ifstream file(input_file);
     std::stringstream ss;
     std::string line, buffer;
     std::string node_file, pl_file, scl_file;
-    int max_displacement;
 
     // Read file
     std::getline(file, line);
     ss << line;
     ss >> buffer >> buffer >> node_file >> pl_file >> scl_file;
     ss.clear();
+
     std::getline(file, line);
     ss << line;
     ss >> buffer >> buffer >> max_displacement;
+    ss.clear();
 
-    std::cout << "node_file: " << node_file << std::endl;
-    std::cout << "pl_file: " << pl_file << std::endl;
-    std::cout << "scl_file: " << scl_file << std::endl;
-    std::cout << "max_displacement: " << max_displacement << std::endl;
+    read_node(path + node_file);
+    read_pl(path + pl_file);
+    read_scl(path + scl_file);
 }
--- HW4/HW4/src/Legalizer.hpp
+++ HW4/HW4/src/Legalizer.hpp
@@ -3,12 +3,19 @@
 class Legalizer {
 public:
     // Variables
+    int max_displacement;
+    int num_nodes;
+    int num_cells;
+    int num_blockages;
 
     // Constructors
     Legalizer();
     Legalizer(std::string input_file, std::string output_file);
 
     // Functions
+    void read_node(std::string node_file);
+    void read_pl(std::string pl_file);
+    void read_scl(std::string scl_file);
     void read_input(std::string input_file);
     void write_result(std::string output_file);
 
